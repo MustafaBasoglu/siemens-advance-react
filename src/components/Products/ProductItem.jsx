3c4ee731f0024e96a31ebcd3625450dc102573e1
eminbasbayan <emin.basbayan@icloud.com>
2024-05-15 10:54:46 +0300

    react component içinde javascript değişkenleri kullanımı

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,17 +1,21 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const image =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdZRDVij3RuXMIOrulPp9Z7CsEryJV8yeCIg&usqp=CAU";
+
+  const title = "Soda";
+  
+  const price = 500;
+
   return (
     <div className="product-item">
       <div className="product-image">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdZRDVij3RuXMIOrulPp9Z7CsEryJV8yeCIg&usqp=CAU"
-          alt="tişört"
-        />
+        <img src={image} alt="tişört" />
       </div>
       <div className="product-info">
-        <strong>Soda</strong>
-        <span>500₺</span>
+        <strong>{title}</strong>
+        <span>{price}₺</span>
       </div>
     </div>
   );
