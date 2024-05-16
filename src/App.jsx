80973638d67f261e315218d40ad24e026751cfec
eminbasbayan <emin.basbayan@icloud.com>
2024-05-16 10:43:23 +0300

    loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { ToastContainer } from "react-toastify";
 import Products from "./components/Products/Products";
 import "react-toastify/dist/ReactToastify.css";
-import Timer from "./components/Timer";
-import { useState } from "react";
 
 function App() {
-  const [isShowTimer, setIsShowTimer] = useState(true);
-  
+
   return (
     <div className="app">
-      <button onClick={() => setIsShowTimer(true)}>Open Timer</button>
-      {isShowTimer && <Timer />}
-      <button onClick={() => setIsShowTimer(false)}>Close Timer</button>
       <Products />
       <ToastContainer />
     </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import NewProductForm from "./NewProductForm";
 import ProductItem from "./ProductItem";
 import "./Products.css";
+import Spinner from "../UI/Spinner";
 
 function Products() {
   const [products, setProducts] = useState([]);
-  const [name, setName] = useState("Emin Başbayan");
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleDeleteProduct(id) {
     const filteredProducts = products.filter((product) => product.id !== id);
@@ -13,33 +14,29 @@ function Products() {
   }
 
   async function fetchProducts() {
+    setIsLoading(true);
+    setProducts([]);
     try {
       const res = await fetch("https://dummyjson.com/products");
       const data = await res.json();
       setProducts(data.products);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
-  // component ilk yüklendiğinde (mount)
   useEffect(() => {
     fetchProducts();
-    document.querySelector(".products").innerHTML = "";
   }, []);
-
-  // component ilk yüklendiğinde ve update olduğunda
-  useEffect(() => {
-    console.log("component update olduğunda!");
-  }, [name]);
 
   return (
     <div className="products-wrapper">
       <h1>Products Component</h1>
-      <p>{name}</p>
-      <button onClick={() => setName("Ahmet")}>Title Change</button>
       <button onClick={fetchProducts}>Ürünleri Getir</button>
       <NewProductForm setProducts={setProducts} />
+      {isLoading && <Spinner />}
       <div className="products">
         {products.map((product) => (
           <ProductItem
