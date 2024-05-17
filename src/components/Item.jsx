d6df4863897b87e24c0854ffc44a3989a143a3b6
eminbasbayan <emin.basbayan@icloud.com>
2024-05-17 16:42:23 +0300

    fixed

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,19 @@
 import PropTypes from "prop-types";
 
 const Item = ({ result }) => {
-
   const itemTextCol = () => (
     <div className="item-column">
+      <img src={result?.thumbnail?.source} alt="" width={200} />
       <h2>
-        <a href="">{result.title}</a>
+        <a target="_blank" href={`https://en.wikipedia.org/?curid=${result.pageid}`}>
+          {result.title}
+        </a>
       </h2>
       <p>{result.extract}</p>
     </div>
   );
 
-  const content = (<article>{itemTextCol()}</article>);
+  const content = <article>{itemTextCol()}</article>;
 
   return content;
 };
