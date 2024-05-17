a7d72c160d83f896295f29ab1b49a2b4bf461c77
eminbasbayan <emin.basbayan@icloud.com>
2024-05-17 14:40:43 +0300

    useCallback dependecy array kullanımı

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,17 +4,27 @@ import Button from "../components/UI/Button";
 
 const HomePage = () => {
   const [toggleParagraph, setToggleParagraph] = useState(false);
+  const [toggleAllow, setToggleAllow] = useState(false);
 
   console.log("HomePage çalıştı!");
 
   const toggleParagraphHandler = useCallback(() => {
-    setToggleParagraph((prevState) => !prevState);
-  }, []);
+    if(toggleAllow){
+      setToggleParagraph((prevState) => !prevState);
+    }
+  }, [toggleAllow]);
+
+  const toggleAllowHandler = () =>{
+    setToggleAllow(true)
+  }
 
   return (
     <div className="home-page">
       <h1>Home Page</h1>
-      <MyElement show={false} />
+      <MyElement show={toggleParagraph} />
+      <br />
+      <Button onClick={toggleAllowHandler}>Toggle Allow</Button>
+      <br />
       <br />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
